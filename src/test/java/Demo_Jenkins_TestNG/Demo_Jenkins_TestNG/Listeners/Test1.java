04566cc1b1204b4a0b9ac4d118bb78bb787aed53
akhila-s-25 <akhila.qa25@gmail.com>
2024-06-07 15:46:42 +0530

    adding test1 file

---
+++ src/test/java/Demo_Jenkins_TestNG/Demo_Jenkins_TestNG/Listeners/Test1.java
@@ -0,0 +1,24 @@
+package Demo_Jenkins_TestNG.Demo_Jenkins_TestNG.Listeners;
+
+import org.testng.annotations.Test;
+
+public class Test1 {
+
+	@Test
+	public void gitmethod() {
+		
+		
+		System.out.println("Git changes");
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+}
